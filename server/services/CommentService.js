c5cb4c21abae986ba407cc77c4f9f556d14cd87a
Geneieve MacKenzie <mackenzie.gen@gmail.com>
2019-09-08 14:49:39 -0600

    fixed schema

--- server/services/CommentService.js
+++ server/services/CommentService.js
@@ -6,9 +6,10 @@ const _model = new Schema({
 
     blogId: { type: ObjectId, ref: "Comment", required: true },
     body: { type: String },
-    // author: { // How could this data get.... populated 
-    //     _id: {type: ObjectId, ref: "User", required: true}, 
-    //     name: "Jim Bob"
+    author: { // How could this data get.... populated 
+        _id: { type: ObjectId, ref: "User", required: true },
+        name: "Jim Bob"
+    }
 }, { timestamps: true })
 
 
